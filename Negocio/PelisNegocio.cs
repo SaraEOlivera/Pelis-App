c21e3c9264493499674fadd236956cbcdb1e09aa
SaraEOlivera <saraeolivera@gmail.com>
2025-02-05 09:41:21 -0300

    Se realiza conexion a la BD

--- Negocio/PelisNegocio.cs
+++ Negocio/PelisNegocio.cs
@@ -3,12 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using Dominio;
 
 namespace Negocio
 {
     internal class PelisNegocio
     {
-        public List<Pelicula> lista = new List<Pelicula>();
+        public List<Pelicula> listar() 
+        {
+            List<Pelicula>listaPelis = new List<Pelicula>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server = .\\SQLEXPRESS; database = MaxiFlix_DB; Integrated Security = true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "Select Titulo, Bio, MinutosDuracion as Duracion from Peliculas";
+                comando.Connection = conexion;
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Pelicula auxiliar = new Pelicula();
+                    auxiliar.Titulo = (string)lector["Titulo"];
+                    auxiliar.Bio = (string)lector["Bio"];
+                    auxiliar.Duracion = (int)lector["MinutosDuracion"];
+
+                    listaPelis.Add(auxiliar);
+                }
+                return listaPelis;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally 
+            {
+                conexion.Close();
+            }
+        }
+
+
+
+
+
+
+
+
     }
 }
--- Presentacion/FormPrincipal.Designer.cs
+++ Presentacion/FormPrincipal.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvPeliculas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPeliculas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvPeliculas
+            // 
+            this.dgvPeliculas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPeliculas.Location = new System.Drawing.Point(77, 53);
+            this.dgvPeliculas.Name = "dgvPeliculas";
+            this.dgvPeliculas.Size = new System.Drawing.Size(240, 150);
+            this.dgvPeliculas.TabIndex = 0;
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(614, 336);
+            this.Controls.Add(this.dgvPeliculas);
             this.Name = "FormPrincipal";
-            this.Text = "Form1";
+            this.Text = "Peliculas";
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPeliculas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvPeliculas;
     }
 }
 
