3202167e02cddd050fcf2653abd15be11583c8a3
SaraEOlivera <saraeolivera@gmail.com>
2025-02-10 22:55:45 -0300

    Formulario de vista detallada con controles

---
+++ Presentacion/frmDetalle.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Presentacion
+{
+    partial class frmDetalle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.lblTituloDetalle = new System.Windows.Forms.Label();
+            this.lblRepartoDetalle = new System.Windows.Forms.Label();
+            this.lblDuracionDetalle = new System.Windows.Forms.Label();
+            this.lblEstrenoDetalle = new System.Windows.Forms.Label();
+            this.lblBioDetalle = new System.Windows.Forms.Label();
+            this.lblPlataformaDetalle = new System.Windows.Forms.Label();
+            this.txtTituloDetalle = new System.Windows.Forms.TextBox();
+            this.txtPlataformasDetalle = new System.Windows.Forms.TextBox();
+            this.txtRepartoDetalle = new System.Windows.Forms.TextBox();
+            this.txtDuracionDetalle = new System.Windows.Forms.TextBox();
+            this.txtEstrenoDetalle = new System.Windows.Forms.TextBox();
+            this.txtBioDetalle = new System.Windows.Forms.TextBox();
+            this.pbxDetalle = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDetalle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(469, 264);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(267, 23);
+            this.btnVolver.TabIndex = 0;
+            this.btnVolver.Text = "&Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // lblTituloDetalle
+            // 
+            this.lblTituloDetalle.AutoSize = true;
+            this.lblTituloDetalle.Location = new System.Drawing.Point(43, 20);
+            this.lblTituloDetalle.Name = "lblTituloDetalle";
+            this.lblTituloDetalle.Size = new System.Drawing.Size(35, 13);
+            this.lblTituloDetalle.TabIndex = 1;
+            this.lblTituloDetalle.Text = "Título";
+            // 
+            // lblRepartoDetalle
+            // 
+            this.lblRepartoDetalle.AutoSize = true;
+            this.lblRepartoDetalle.Location = new System.Drawing.Point(43, 216);
+            this.lblRepartoDetalle.Name = "lblRepartoDetalle";
+            this.lblRepartoDetalle.Size = new System.Drawing.Size(45, 13);
+            this.lblRepartoDetalle.TabIndex = 2;
+            this.lblRepartoDetalle.Text = "Reparto";
+            // 
+            // lblDuracionDetalle
+            // 
+            this.lblDuracionDetalle.AutoSize = true;
+            this.lblDuracionDetalle.Location = new System.Drawing.Point(35, 165);
+            this.lblDuracionDetalle.Name = "lblDuracionDetalle";
+            this.lblDuracionDetalle.Size = new System.Drawing.Size(50, 13);
+            this.lblDuracionDetalle.TabIndex = 3;
+            this.lblDuracionDetalle.Text = "Duración";
+            // 
+            // lblEstrenoDetalle
+            // 
+            this.lblEstrenoDetalle.AutoSize = true;
+            this.lblEstrenoDetalle.Location = new System.Drawing.Point(35, 113);
+            this.lblEstrenoDetalle.Name = "lblEstrenoDetalle";
+            this.lblEstrenoDetalle.Size = new System.Drawing.Size(43, 13);
+            this.lblEstrenoDetalle.TabIndex = 4;
+            this.lblEstrenoDetalle.Text = "Estreno";
+            // 
+            // lblBioDetalle
+            // 
+            this.lblBioDetalle.AutoSize = true;
+            this.lblBioDetalle.Location = new System.Drawing.Point(54, 69);
+            this.lblBioDetalle.Name = "lblBioDetalle";
+            this.lblBioDetalle.Size = new System.Drawing.Size(24, 13);
+            this.lblBioDetalle.TabIndex = 5;
+            this.lblBioDetalle.Text = "Bío";
+            // 
+            // lblPlataformaDetalle
+            // 
+            this.lblPlataformaDetalle.AutoSize = true;
+            this.lblPlataformaDetalle.Location = new System.Drawing.Point(35, 264);
+            this.lblPlataformaDetalle.Name = "lblPlataformaDetalle";
+            this.lblPlataformaDetalle.Size = new System.Drawing.Size(62, 13);
+            this.lblPlataformaDetalle.TabIndex = 6;
+            this.lblPlataformaDetalle.Text = "Plataformas";
+            // 
+            // txtTituloDetalle
+            // 
+            this.txtTituloDetalle.Location = new System.Drawing.Point(165, 20);
+            this.txtTituloDetalle.Name = "txtTituloDetalle";
+            this.txtTituloDetalle.ReadOnly = true;
+            this.txtTituloDetalle.Size = new System.Drawing.Size(259, 20);
+            this.txtTituloDetalle.TabIndex = 7;
+            // 
+            // txtPlataformasDetalle
+            // 
+            this.txtPlataformasDetalle.Location = new System.Drawing.Point(165, 264);
+            this.txtPlataformasDetalle.Name = "txtPlataformasDetalle";
+            this.txtPlataformasDetalle.ReadOnly = true;
+            this.txtPlataformasDetalle.Size = new System.Drawing.Size(259, 20);
+            this.txtPlataformasDetalle.TabIndex = 8;
+            // 
+            // txtRepartoDetalle
+            // 
+            this.txtRepartoDetalle.Location = new System.Drawing.Point(165, 216);
+            this.txtRepartoDetalle.Name = "txtRepartoDetalle";
+            this.txtRepartoDetalle.ReadOnly = true;
+            this.txtRepartoDetalle.Size = new System.Drawing.Size(259, 20);
+            this.txtRepartoDetalle.TabIndex = 9;
+            // 
+            // txtDuracionDetalle
+            // 
+            this.txtDuracionDetalle.Location = new System.Drawing.Point(165, 162);
+            this.txtDuracionDetalle.Name = "txtDuracionDetalle";
+            this.txtDuracionDetalle.ReadOnly = true;
+            this.txtDuracionDetalle.Size = new System.Drawing.Size(259, 20);
+            this.txtDuracionDetalle.TabIndex = 10;
+            // 
+            // txtEstrenoDetalle
+            // 
+            this.txtEstrenoDetalle.Location = new System.Drawing.Point(165, 110);
+            this.txtEstrenoDetalle.Name = "txtEstrenoDetalle";
+            this.txtEstrenoDetalle.ReadOnly = true;
+            this.txtEstrenoDetalle.Size = new System.Drawing.Size(259, 20);
+            this.txtEstrenoDetalle.TabIndex = 11;
+            // 
+            // txtBioDetalle
+            // 
+            this.txtBioDetalle.Location = new System.Drawing.Point(165, 69);
+            this.txtBioDetalle.Name = "txtBioDetalle";
+            this.txtBioDetalle.ReadOnly = true;
+            this.txtBioDetalle.Size = new System.Drawing.Size(259, 20);
+            this.txtBioDetalle.TabIndex = 12;
+            // 
+            // pbxDetalle
+            // 
+            this.pbxDetalle.Location = new System.Drawing.Point(469, 20);
+            this.pbxDetalle.Name = "pbxDetalle";
+            this.pbxDetalle.Size = new System.Drawing.Size(267, 233);
+            this.pbxDetalle.TabIndex = 13;
+            this.pbxDetalle.TabStop = false;
+            // 
+            // frmDetalle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 312);
+            this.Controls.Add(this.pbxDetalle);
+            this.Controls.Add(this.txtBioDetalle);
+            this.Controls.Add(this.txtEstrenoDetalle);
+            this.Controls.Add(this.txtDuracionDetalle);
+            this.Controls.Add(this.txtRepartoDetalle);
+            this.Controls.Add(this.txtPlataformasDetalle);
+            this.Controls.Add(this.txtTituloDetalle);
+            this.Controls.Add(this.lblPlataformaDetalle);
+            this.Controls.Add(this.lblBioDetalle);
+            this.Controls.Add(this.lblEstrenoDetalle);
+            this.Controls.Add(this.lblDuracionDetalle);
+            this.Controls.Add(this.lblRepartoDetalle);
+            this.Controls.Add(this.lblTituloDetalle);
+            this.Controls.Add(this.btnVolver);
+            this.Name = "frmDetalle";
+            this.Text = "Información Detallada";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDetalle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Label lblTituloDetalle;
+        private System.Windows.Forms.Label lblRepartoDetalle;
+        private System.Windows.Forms.Label lblDuracionDetalle;
+        private System.Windows.Forms.Label lblEstrenoDetalle;
+        private System.Windows.Forms.Label lblBioDetalle;
+        private System.Windows.Forms.Label lblPlataformaDetalle;
+        private System.Windows.Forms.TextBox txtTituloDetalle;
+        private System.Windows.Forms.TextBox txtPlataformasDetalle;
+        private System.Windows.Forms.TextBox txtRepartoDetalle;
+        private System.Windows.Forms.TextBox txtDuracionDetalle;
+        private System.Windows.Forms.TextBox txtEstrenoDetalle;
+        private System.Windows.Forms.TextBox txtBioDetalle;
+        private System.Windows.Forms.PictureBox pbxDetalle;
+    }
+}
