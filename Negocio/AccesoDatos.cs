4a2b89305c4f3cc0d04a9942407240d897d2a939
SaraEOlivera <saraeolivera@gmail.com>
2025-02-10 22:17:28 -0300

    Ultimas modificaciones a la clase AccesoDatos

--- Negocio/AccesoDatos.cs
+++ Negocio/AccesoDatos.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Negocio
 {
-    internal class AccesoDatos
+    public class AccesoDatos
     {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector  {
+            get { return Lector;  }
+        }
+        public AccesoDatos() 
+        {
+            conexion = new SqlConnection("server = .\\SQLEXPRESS; database = MaxiFlix_DB; Integrated Security = true");
+            comando = new SqlCommand();
+        }
+
+        #region setearConsulta
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+        #endregion
+
+        #region ejecutarConsulta
+        public void ejecutarConsulta()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        #endregion
+
+        #region cerrarConexion
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        } 
+        #endregion
     }
 }
